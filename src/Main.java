2ba4645d9da8dc28c699c92670d0f58f1b43f53a
Андрей <ustu1982@mail.ru>
2023-07-03 10:38:54 +0500

    end

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car car = new Car("Audi", 4);
+        Car car2 = new Car("BMW", 4);
+        Truck truck = new Truck("Volvo", 8);
+        Truck truck2 = new Truck("Man", 6);
+        Bicycle bicycle = new Bicycle("КАМА", 2);
+        Bicycle bicycle2 = new Bicycle("УРАЛ", 2);
+
+        ServiceStation station = new ServiceStation();
+        station.check(car);
+        station.check(bicycle);
+        station.check(truck);
+        station.check(truck2);
     }
 }
